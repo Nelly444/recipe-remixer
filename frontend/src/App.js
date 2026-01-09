c9ea63e564159e0700575c4c4780c7b178b9f6fc
Nelly444 <nelsonsupriyasilp111@gmail.com>
2026-01-08 19:31:05 -0700

    Debugged frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,7 +6,7 @@ function App() {
   useEffect(() => {
     fetch('http://127.0.0.1:5000/recipes')
       .then(response => response.json())
-      .then(data => setRecipes(data))
+      .then(data => setRecipes(data.recipes))
   }, []);
 
   return (
